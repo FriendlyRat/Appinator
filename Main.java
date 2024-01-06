4fa90e73d0ca084031023c6727ded51cb12a8b94
FriendlyRat <adebowalem@gmail.com>
2024-01-06 15:56:25 -0500

    First proof of run

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*
+Project: TBD
+Created by: Ife Odeleye, Mateo Adebowale
+Description:
+This is the main file housing all our primary code.
+*/
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
